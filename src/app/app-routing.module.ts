b431a84eb4ea32bcc566e99ea8efefeea19c6532
Fransiskaindri <fransiskaindri15@gmail.com>
2021-10-29 17:14:04 +0700

    Ini Projek Game-ku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { ProfilComponent } from './profil/profil.component';
+import { TournamentComponent } from './tournament/tournament.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:"Profil",component:ProfilComponent},
+ {path:"Tournament",component:TournamentComponent},
+ {path:"Leaderboard",component:LeaderboardComponent},
+ {path:"Home",component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { ProfilComponent } from './profil/profil.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TournamentComponent,
+    ProfilComponent,
+    LeaderboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
